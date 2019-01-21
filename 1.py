a11a01594ea94504c15b4f1441e63c4776cce0fb
vlukinskii <vlukinskii@bk.ru>
2019-01-21 10:43:32 +0500

    Добавлен файл gitignore

--- 1.py
+++ 1.py
@@ -1,11 +1,25 @@
 from graph import *
-x=int(input())
-y=int(input())
-R=int(input())
-a=circle(x,y,R)
+canvasSize(500,500)
+class shar:
+    def __init__(self):
+        self.X=''
+        self.Y=''
+        self.moveX=''
+        self.moveY=''
+        self.rad=''
+        self.cvet=''
+        self.circle=''
+r=int(input())
+dx=int(input())
+dy=int(input())
+a=circle(r,50,r)
 def update():
-    dx=1
-    dy=1
     moveObjectBy(a,dx,dy)
+    x=xCoord(a)
+    y=yCoord(a)
+    if x==500-r*2:
+        dx=-dx
+    if y==500-r*2:
+        dy=-dy
 onTimer(update,10)
 run()
